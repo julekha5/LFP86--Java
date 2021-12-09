111b2c12dd5e1e8b4b1f47223d7dc4f3ccdeb8d0
Julekha5 <mulani.julekha93@gmail.com>
2021-12-09 16:00:23 +0530

    Addded:Multiple Company EmpWage Using Interface

--- src/day10/EmpWageBuilder.java
+++ src/day10/EmpWageBuilder.java
@@ -1,65 +1,56 @@
 package day10;
-//UC9- Save total Wage for each company
-	public class EmpWageBuilder {
-	    // Declaring Constants
-	    public static final int IS_PART_TIME = 1;
-	    public static final int IS_FULL_TIME = 2;
-	    public static final int FULL_DAY_HR = 8;
-	    public static final int PART_DAY_HR = 4;
-	    // Declaring Variable
-	    private static String companyName;
-	    private static int empWagePerHr;
-	    private static int numMaxWorkingDay;
-	    private static int maxHrsInMonth;
-	    private static int totalEmpWage;
 
-	    // Constructor
-	    public EmpWageBuilder(String companyName, int empWagePerHr, int numMaxWorkingDay, int maxHrsInMonth) {
-	        this.companyName = companyName;
-	        this.empWagePerHr = empWagePerHr;
-	        this.numMaxWorkingDay = numMaxWorkingDay;
-	        this.maxHrsInMonth = maxHrsInMonth;
-	    }
+//Employee wage for Multiple Company using interface approach
+public class EmpWageBuilder {
+	// Declaring Constants
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+	public static final int FULL_DAY_HR = 8;
+	public static final int PART_DAY_HR = 4;
 
-	    public EmpWageBuilder() {
-			// TODO Auto-generated constructor stub
-		}
+	private CompanyEmpWage[] companyEmpWagerArray = new CompanyEmpWage[5];
+	private int numOfCompany = 0;
 
-		@Override
-	    public String toString() {
-	        return "Total EmpWAge for " + companyName + "  " + totalEmpWage;
-	    }
-
-	    public static void main(String[] args) {
-	        EmpWageBuilder dMartEmpWage = new EmpWageBuilder("dMart", 20, 20, 100);
-	        dMartEmpWage.computeEmpWage();
-	        System.out.println(dMartEmpWage);
-	        EmpWageBuilder tcsEmpWage = new EmpWageBuilder("tcs", 16, 20, 100);
-	        tcsEmpWage.computeEmpWage();
-	        System.out.println(tcsEmpWage);
-	    }
-
-	    private void computeEmpWage() {
-	        int totalEmpHours = 0, totalWorkingDays = 0, workingHours = 0;
-	        while (totalEmpHours < maxHrsInMonth && totalWorkingDays < numMaxWorkingDay) {
-	            totalWorkingDays++;
-	            double empCheck = Math.floor(Math.random() * 10) % 3;
-	            // Calculating Wage On Employee Type
-	            switch ((int) empCheck) {
-	                case 0:
-	                    workingHours = FULL_DAY_HR;
-	                    break;
-	                case 1:
-	                    workingHours = PART_DAY_HR;
-	                    break;
-	                default:
-	                    workingHours = 0;
-	            }
-	            totalEmpHours += workingHours;
-	            System.out.println("Day: " + totalWorkingDays + " workingHours: " + workingHours);
-	        }
-	        //Calculate Employ wage
-	        totalEmpWage = totalEmpHours * empWagePerHr;
-	    }
+	public static void main(String[] args) {
+		EmpWageBuilder empWageBuilder = new EmpWageBuilder();
+		empWageBuilder.addCompanyEmpWage("TCS", 20, 4, 10);
+		empWageBuilder.addCompanyEmpWage("dMart", 20, 14, 20);
+		empWageBuilder.computeEmpWageFromArray();
 	}
 
+	private void addCompanyEmpWage(String companyName, int empWagePerHr, int numMaxWorkingDay, int maxHrsInMonth) {
+		CompanyEmpWage empWage = new CompanyEmpWage(companyName, empWagePerHr, numMaxWorkingDay, maxHrsInMonth);
+		companyEmpWagerArray[numOfCompany] = empWage;
+		numOfCompany++;
+	}
+
+	private void computeEmpWageFromArray() {
+		for (int i = 0; i < numOfCompany; i++) {
+			int totalWage = computeEmpWage(companyEmpWagerArray[i]);
+			companyEmpWagerArray[i].totalEmpWage = totalWage;
+			System.out.println(companyEmpWagerArray[i]);
+		}
+	}
+
+	private int computeEmpWage(CompanyEmpWage companyEmpWage) {
+		int totalEmpHours = 0, totalWorkingDays = 0, workingHours = 0;
+		while (totalEmpHours < companyEmpWage.maxHrsInMonth && totalWorkingDays < companyEmpWage.numMaxWorkingDay) {
+			totalWorkingDays++;
+			double empCheck = Math.floor(Math.random() * 10) % 3;
+			// Calculating Wage On Employee Type
+			switch ((int) empCheck) {
+			case 0:
+				workingHours = FULL_DAY_HR;
+				break;
+			case 1:
+				workingHours = PART_DAY_HR;
+				break;
+			default:
+				workingHours = 0;
+			}
+			totalEmpHours += workingHours;
+			totalWorkingDays++;
+		}
+		return totalEmpHours * companyEmpWage.empWagePerHr;
+	}
+}
